6cf2a4d0fc0d850ff17158e22a5cb532472c2b46
Akhila <vempadapuakhila@gmail.com>
2024-04-17 23:35:47 +0530

    empty change

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-// import Home from './home';
+import Home from './home';
 import Text from './txt';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Home/> */}
+    <Home/>
     <Text/>
     {/* <App /> */}
   </React.StrictMode>
